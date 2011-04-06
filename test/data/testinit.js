2b5e49bd22653d918ea8a4e2962ce90ed4ad10e3
gnarf <gnarf@gnarf.net>
2011-04-06 16:27:53 -0500

    Stripping some copy-paste job from the testinit

--- test/data/testinit.js
+++ test/data/testinit.js
@@ -1,36 +1,4 @@
 var jQuery = this.jQuery || "jQuery", // For testing .noConflict()
-	$ = this.$ || "$",
-	originaljQuery = jQuery,
-	original$ = $;
-
-/**
- * Returns an array of elements with the given IDs, eg.
- * @example q("main", "foo", "bar")
- * @result [<div id="main">, <span id="foo">, <input id="bar">]
- */
-function q() {
-	var r = [];
-
-	for ( var i = 0; i < arguments.length; i++ ) {
-		r.push( document.getElementById( arguments[i] ) );
-	}
-
-	return r;
-}
-
-/**
- * Asserts that a select matches the given IDs * @example t("Check for something", "//[a]", ["foo", "baar"]);
- * @result returns true if "//[a]" return two elements with the IDs 'foo' and 'baa
-r'
- */
-function t(a,b,c) {
-	var f = jQuery(b).get(), s = "";
-
-	for ( var i = 0; i < f.length; i++ ) {
-		s += (s && ",") + '"' + f[i].id + '"';
-	}
-
-	same(f, q.apply(q,c), a + " (" + b + ")");
-}
+	$ = this.$ || "$";
 
 function moduleTeardown(){}
