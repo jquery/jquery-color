33af26aca401a8cd97a23506738d169d93f98bff
Brendan Byrd <Perl@ResonatorSoft.org>
2012-07-04 23:20:29 -0400

    Add all 147 HTML/CSS legal color names into new SVG-names JS file Strip out non-basic color names from original jquery.color.js file

--- jquery.color.js
+++ jquery.color.js
@@ -604,55 +604,32 @@ each( stepHooks, function( i, hook ) {
 	};
 });
 
-// Some named colors to work with
-// From Interface by Stefan Petre
-// http://interface.eyecon.ro/
+// Just the basic color keywords + transparent.
+// Usage of any of the other colors requires
+// jquery.color.svg-names.js.
 colors = jQuery.Color.names = {
-	aqua: "#00ffff",
-	azure: "#f0ffff",
-	beige: "#f5f5dc",
-	black: "#000000",
-	blue: "#0000ff",
-	brown: "#a52a2a",
-	cyan: "#00ffff",
-	darkblue: "#00008b",
-	darkcyan: "#008b8b",
-	darkgrey: "#a9a9a9",
-	darkgreen: "#006400",
-	darkkhaki: "#bdb76b",
-	darkmagenta: "#8b008b",
-	darkolivegreen: "#556b2f",
-	darkorange: "#ff8c00",
-	darkorchid: "#9932cc",
-	darkred: "#8b0000",
-	darksalmon: "#e9967a",
-	darkviolet: "#9400d3",
-	fuchsia: "#ff00ff",
-	gold: "#ffd700",
-	green: "#008000",
-	indigo: "#4b0082",
-	khaki: "#f0e68c",
-	lightblue: "#add8e6",
-	lightcyan: "#e0ffff",
-	lightgreen: "#90ee90",
-	lightgrey: "#d3d3d3",
-	lightpink: "#ffb6c1",
-	lightyellow: "#ffffe0",
-	lime: "#00ff00",
-	magenta: "#ff00ff",
-	maroon: "#800000",
-	navy: "#000080",
-	olive: "#808000",
-	orange: "#ffa500",
-	pink: "#ffc0cb",
-	purple: "#800080",
-	violet: "#800080",
-	red: "#ff0000",
-	silver: "#c0c0c0",
-	white: "#ffffff",
-	yellow: "#ffff00",
-	transparent: [ null, null, null, 0 ],
-	_default: "#ffffff"
+   // 4.1. Basic color keywords
+   black:       '#000000',
+   silver:      '#c0c0c0',
+   gray:        '#808080',
+   white:       '#ffffff',
+   maroon:      '#800000',
+   red:         '#ff0000',
+   purple:      '#800080',
+   fuchsia:     '#ff00ff',
+   green:       '#008000',
+   lime:        '#00ff00',
+   olive:       '#808000',
+   yellow:      '#ffff00',
+   navy:        '#000080',
+   blue:        '#0000ff',
+   teal:        '#008080',
+   aqua:        '#00ffff',
+
+   // 4.2.3. ‘transparent’ color keyword
+   transparent: [ null, null, null, 0 ],
+
+	_default:    "#ffffff"
 };
 
 })( jQuery );
