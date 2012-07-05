a98c3be8cccf90bc44894775570687762035de1d
Corey Frang <gnarf@gnarf.net>
2012-07-05 11:21:24 -0500

    Alphabetic ordering for basic colors

--- jquery.color.js
+++ jquery.color.js
@@ -609,22 +609,22 @@ each( stepHooks, function( i, hook ) {
 // jquery.color.svg-names.js.
 colors = jQuery.Color.names = {
 	// 4.1. Basic color keywords
+	aqua: "#00ffff",
 	black: "#000000",
-	silver: "#c0c0c0",
-	gray: "#808080",
-	white: "#ffffff",
-	maroon: "#800000",
-	red: "#ff0000",
-	purple: "#800080",
+	blue: "#0000ff",
 	fuchsia: "#ff00ff",
+	gray: "#808080",
 	green: "#008000",
 	lime: "#00ff00",
-	olive: "#808000",
-	yellow: "#ffff00",
+	maroon: "#800000",
 	navy: "#000080",
-	blue: "#0000ff",
+	olive: "#808000",
+	purple: "#800080",
+	red: "#ff0000",
+	silver: "#c0c0c0",
 	teal: "#008080",
-	aqua: "#00ffff",
+	white: "#ffffff",
+	yellow: "#ffff00",
 
 	// 4.2.3. ‘transparent’ color keyword
 	transparent: [ null, null, null, 0 ],
