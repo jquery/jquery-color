41910896f48b83389c22673e76ba719f7765edfe
Ben Olson <bseth99@yahoo.com>
2012-10-21 18:50:34 -0400

    Fix comments

--- jquery.color.js
+++ jquery.color.js
@@ -462,8 +462,8 @@ spaces.hsla.to = function ( rgba ) {
 		h = ( 60 * ( r - g ) / diff ) + 240;
 	}
 
-	// diff (chroma) == 0 means greyscale which, by definition, saturation = 0%
-	// otherwise, saturation is based on the ratio of chroma to lightness
+	// chroma (diff) == 0 means greyscale which, by definition, saturation = 0%
+	// otherwise, saturation is based on the ratio of chroma (diff) to lightness (add)
 	if ( diff === 0 ) {
 		s = 0;
 	} else if ( l <= 0.5 ) {
