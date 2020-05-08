35893b7958225a5b8a95b243fc16709f70a43416
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2020-05-08 21:35:40 +0200

    Build: Add a jQuery version dropdown

---
+++ test/data/testinit.js
@@ -0,0 +1,13 @@
+"use strict";
+
+QUnit.config.urlConfig.push( {
+	id: "jquery",
+	label: "jQuery version",
+	value: [
+		"1.5.2", "1.6.4", "1.7.2", "1.8.3", "1.9.1", "1.10.2", "1.11.3", "1.12.4",
+		"2.0.3", "2.1.4", "2.2.4",
+		"3.0.0", "3.1.1", "3.2.1", "3.3.1", "3.4.1", "3.5.1",
+		"3.x-git", "git"
+	],
+	tooltip: "Which jQuery Core version to test against"
+} );
