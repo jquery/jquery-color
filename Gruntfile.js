f4d0fd8a95b3f261a0795e511f347e2e60ebdfe5
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2020-05-10 00:36:28 +0200

    Build: Remove a bad `@` symbol from minified version sources.
    We want v1.2.3 in both the unminified & minified builds, not v@1.2.3.

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,19 +8,19 @@ var max = [ "dist/jquery.color.js", "dist/jquery.color.svg-names.js" ],
 	minify = {
 		main: {
 			options: {
-				banner: "/*! jQuery Color v@<%= pkg.version %> http://github.com/jquery/jquery-color | jquery.org/license */\n"
+				banner: "/*! jQuery Color v<%= pkg.version %> http://github.com/jquery/jquery-color | jquery.org/license */\n"
 			},
 			files: {}
 		},
 		svg: {
 			options: {
-				banner: "/*! jQuery Color v@<%= pkg.version %> SVG Color Names http://github.com/jquery/jquery-color | jquery.org/license */\n"
+				banner: "/*! jQuery Color v<%= pkg.version %> SVG Color Names http://github.com/jquery/jquery-color | jquery.org/license */\n"
 			},
 			files: {}
 		},
 		combined: {
 			options: {
-				banner: "/*! jQuery Color v@<%= pkg.version %> with SVG Color Names http://github.com/jquery/jquery-color | jquery.org/license */\n"
+				banner: "/*! jQuery Color v<%= pkg.version %> with SVG Color Names http://github.com/jquery/jquery-color | jquery.org/license */\n"
 			},
 			files: {}
 		}
