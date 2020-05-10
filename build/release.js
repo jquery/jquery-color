57a7d188e0db82e8791d4790ba9c4443851d34eb
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2020-05-10 22:41:49 +0200

    Build: Put version in file names instead of the parent directory
    (cherry picked from commit 4ba7bedae95adab83b9545283307725d17b0c31a)

--- build/release.js
+++ build/release.js
@@ -1,20 +1,45 @@
 "use strict";
 
 module.exports = function( Release ) {
-	var
-		files = [
+	var shell = require( "shelljs" ),
+		cdnFolder = "dist/cdn",
+		distFiles = [
 			"dist/jquery.color.js",
 			"dist/jquery.color.min.js",
 			"dist/jquery.color.plus-names.js",
 			"dist/jquery.color.plus-names.min.js",
 			"dist/jquery.color.svg-names.js",
 			"dist/jquery.color.svg-names.min.js"
-		];
+		],
+		releaseFiles = {
+			"jquery.color-@VERSION.js": "dist/jquery.color.js",
+			"jquery.color-@VERSION.min.js": "dist/jquery.color.min.js",
+			"jquery.color.plus-names-@VERSION.js": "dist/jquery.color.plus-names.js",
+			"jquery.color.plus-names-@VERSION.min.js": "dist/jquery.color.plus-names.min.js",
+			"jquery.color.svg-names-@VERSION.js": "dist/jquery.color.svg-names.js",
+			"jquery.color.svg-names-@VERSION.min.js": "dist/jquery.color.svg-names.min.js"
+		};
+
+	function makeReleaseCopies( Release ) {
+		shell.mkdir( "-p", cdnFolder );
+
+		Object.keys( releaseFiles ).forEach( function( key ) {
+			var builtFile = releaseFiles[ key ],
+				unpathedFile = key.replace( /@VERSION/g, Release.newVersion ),
+				releaseFile = cdnFolder + "/" + unpathedFile;
+
+			shell.cp( "-f", builtFile, releaseFile );
+		} );
+	}
 
 	Release.define( {
 		npmPublish: true,
 		issueTracker: "github",
-		cdnPublish: "dist",
+		cdnPublish: cdnFolder,
+
+		changelogShell: function() {
+			return "# Changelog for jQuery Color v" + Release.newVersion + "\n";
+		},
 
 		/**
 		 * Generates any release artifacts that should be included in the release.
@@ -24,7 +49,12 @@ module.exports = function( Release ) {
 		 */
 		generateArtifacts: function( callback ) {
 			Release.exec( "grunt", "Grunt command failed" );
-			callback( files );
+			makeReleaseCopies( Release );
+			callback( distFiles );
 		}
 	} );
 };
+
+module.exports.dependencies = [
+	"shelljs@0.8.4"
+];
